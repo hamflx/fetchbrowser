0859449f490015582ac36ad845c6bc41886ee9bc
hamflx <a@hamflx.cn>
2023-01-13 22:56:06 +0800

    feat: 使用 history.json 和 deps.json 替代 fetch_releases 以解决老的版本没有 position 问题。

--- src/main.rs
+++ src/main.rs
@@ -15,19 +15,30 @@ fn main() {
 
     let builds = get_build_list();
 
-    let release_list = get_release_list();
-    let release_info = find_release(release_list.iter(), &ver).unwrap();
+    let history_list = get_history_list();
+    let matched_history_list = find_history(history_list.iter(), &ver);
+
+    let deps = matched_history_list
+        .iter()
+        .find_map(|history| Some(fetch_deps(&history.version)))
+        .expect("No matched version");
+    println!("==> deps: {:#?}", deps);
 
     let (prefix, revision) =
-        find_builds(builds.iter(), release_info.chromium_main_branch_position).unwrap();
+        find_builds(builds.iter(), deps.chromium_base_position.parse().unwrap()).unwrap();
     let build_files = fetch_build_detail(prefix);
     let win_zip = build_files
-        .into_iter()
+        .iter()
         .find(|f| f.name.ends_with("chrome-win.zip"))
+        .or_else(|| {
+            build_files
+                .iter()
+                .find(|f| f.name.ends_with("chrome-win32.zip"))
+        })
         .unwrap();
 
     println!("==> downloading {}", win_zip.media_link);
-    let mut win_zip_response = reqwest::blocking::get(win_zip.media_link).unwrap();
+    let mut win_zip_response = reqwest::blocking::get(&win_zip.media_link).unwrap();
 
     let base_path = std::env::current_dir()
         .unwrap()
@@ -86,6 +97,25 @@ fn get_build_list() -> Vec<String> {
     }
 }
 
+fn get_history_list() -> Vec<ChromiumHistoryInfo> {
+    let history_json_path = get_cached_file_path("history.json");
+    if std::fs::try_exists(&history_json_path).unwrap_or_default() {
+        println!("==> using cached history.");
+        serde_json::from_reader(BufReader::new(File::open(&history_json_path).unwrap())).unwrap()
+    } else {
+        println!("==> retrieving history.json ...");
+        let url = "https://omahaproxy.appspot.com/history.json?os=win&channel=stable";
+        let response = reqwest::blocking::get(url).unwrap();
+        let history_list: Vec<ChromiumHistoryInfo> = serde_json::from_reader(response).unwrap();
+        std::fs::write(
+            &history_json_path,
+            serde_json::to_string(&history_list).unwrap(),
+        )
+        .unwrap();
+        history_list
+    }
+}
+
 fn get_cached_file_path(file: &str) -> PathBuf {
     let mut path = PathBuf::new();
     path.push(std::env::var("LOCALAPPDATA").unwrap());
@@ -97,25 +127,6 @@ fn get_cached_file_path(file: &str) -> PathBuf {
     path
 }
 
-fn get_release_list() -> Vec<ChromiumReleaseInfo> {
-    let releases_json_path = get_cached_file_path("releases.json");
-    if std::fs::try_exists(&releases_json_path).unwrap_or_default() {
-        println!("==> using cached releases.");
-        serde_json::from_reader(std::fs::File::open(&releases_json_path).unwrap()).unwrap()
-    } else {
-        println!("==> retrieving releases ...");
-        let url = "https://chromiumdash.appspot.com/fetch_releases";
-        let response = reqwest::blocking::get(url).unwrap();
-        let release_list: Vec<ChromiumReleaseInfo> = serde_json::from_reader(response).unwrap();
-        std::fs::write(
-            &releases_json_path,
-            serde_json::to_string(&release_list).unwrap(),
-        )
-        .unwrap();
-        release_list
-    }
-}
-
 fn fetch_build_detail(prefix: &str) -> Vec<GoogleApiStorageObject> {
     let url = format!("https://www.googleapis.com/storage/v1/b/chromium-browser-snapshots/o?delimiter=/&prefix={prefix}&fields=items(kind,mediaLink,metadata,name,size,updated),kind,prefixes,nextPageToken");
     let response = reqwest::blocking::get(url).unwrap();
@@ -123,12 +134,20 @@ fn fetch_build_detail(prefix: &str) -> Vec<GoogleApiStorageObject> {
     build_detail.items
 }
 
-fn find_release<'a>(
-    mut release_list: Iter<'a, ChromiumReleaseInfo>,
+fn fetch_deps(version: &str) -> ChromiumDepsInfo {
+    let url = format!("https://omahaproxy.appspot.com/deps.json?version={version}");
+    let response = reqwest::blocking::get(url).unwrap();
+    serde_json::from_reader(response).unwrap()
+}
+
+fn find_history<'a>(
+    history_list: Iter<'a, ChromiumHistoryInfo>,
     ver: &str,
-) -> Option<&'a ChromiumReleaseInfo> {
+) -> Vec<&'a ChromiumHistoryInfo> {
     let prefix = format!("{}.", ver);
-    release_list.find(|info| info.version.starts_with(&prefix))
+    history_list
+        .filter(|info| info.version.starts_with(&prefix))
+        .collect()
 }
 
 fn find_builds<'a>(build_list: Iter<'a, String>, find_pos: usize) -> Option<(&'a String, usize)> {
@@ -214,10 +233,31 @@ pub(crate) struct GoogleApiStorageObject {
 #[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct ChromiumReleaseInfo {
     channel: String,
-    chromium_main_branch_position: usize,
+    chromium_main_branch_position: Option<usize>,
     milestone: usize,
     platform: String,
     previous_version: String,
     time: usize,
     version: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct ChromiumHistoryInfo {
+    channel: String,
+    os: String,
+    timestamp: String,
+    version: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct ChromiumDepsInfo {
+    chromium_base_commit: String,
+    chromium_base_position: String,
+    chromium_branch: String,
+    chromium_commit: String,
+    chromium_version: String,
+    skia_commit: String,
+    v8_commit: String,
+    v8_position: String,
+    v8_version: String,
 }
